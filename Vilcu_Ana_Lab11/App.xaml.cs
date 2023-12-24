43558ffd158b55119118b04e553a86bd4a978c98
anutzaa <ana.vilcu@stud.ubbcluj.ro>
2023-12-24 16:06:13 +0200

    Medii Laborator 11

---
+++ Vilcu_Ana_Lab11/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Vilcu_Ana_Lab11.Data;
+
+namespace Vilcu_Ana_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase? Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Vilcu_Ana_Lab11/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Vilcu_Ana_Lab11
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
